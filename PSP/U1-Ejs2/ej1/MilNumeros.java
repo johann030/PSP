85cd25bc11a2a324849b37586d0cf098710e8196
alumno <alumno@dam206>
2024-10-22 12:32:27 +0200

    Intentar terminar el ej3 de la u2 con procesos

--- PSP/U1-Ejs2/ej1/MilNumeros.java
+++ PSP/U1-Ejs2/ej1/MilNumeros.java
@@ -1,23 +1,15 @@
 package ej1;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MilNumeros {
+	//TODO
 
 	public static void main(String[] args) {
-		/*
-		 * Realizar un programa que permita sumar los datos de un fichero dado
-		 * “numeros.dat” que contiene 1000 números enteros, cada uno de estos números se
-		 * encontrará en una línea diferente. El usuario deberá decidir desde qué número
-		 * hasta qué número debe sumar el programa, por lo que estos dos valores se
-		 * pedirán por teclado. El programa principal mostrará la suma total.
-		 */
 		String lineas;
 		BufferedReader br = null;
 		int num1;
@@ -32,7 +24,6 @@ public class MilNumeros {
 			num1 = sc.nextInt();
 
 			num2 = sc.nextInt();
-			System.out.println("dentro hijo");
 			while ((lineas = br.readLine()) != null) {
 				if((cont >= num1) && (cont <= num2)) {
 					suma = Integer.parseInt(lineas);
---
+++ PSP/U1-Ejs2/ej3/SumaContabilidadMain.java
@@ -0,0 +1,10 @@
+package ej3;
+
+public class SumaContabilidadMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
